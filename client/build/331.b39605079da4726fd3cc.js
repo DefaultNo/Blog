be9116273b8e35e68259d978c525f081120f43c0
DefaultNo <101729312+DefaultNo@users.noreply.github.com>
2023-06-24 14:50:38 +0300

    Delete client/build directory

--- client/build/331.b39605079da4726fd3cc.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[331],{331:(e,n,c)=>{c.r(n),c.d(n,{default:()=>i});var t=c(893);const i=function(){return(0,t.jsx)("div",{children:"MainPage"})}}}]);
